57e152b6876a9571e00d08b09c8160f8ecc57370
pauline <pauline.contat00@gmail.com>
2024-04-30 16:20:32 +0200

    projet propre

---
+++ src/main/java/com/example/ctcd/CtcdApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtcdApplication.class, args);
+	}
+
+}
