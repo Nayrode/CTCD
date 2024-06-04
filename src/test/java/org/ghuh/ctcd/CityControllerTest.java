af82d79030c058e3cb5526d447161bf51e5f6805
pauline <pauline.contat00@gmail.com>
2024-06-04 08:30:38 +0200

    fix in progress of the build

--- src/test/java/org/ghuh/ctcd/CityControllerTest.java
+++ src/test/java/org/ghuh/ctcd/CityControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -20,7 +22,8 @@ import static org.mockito.Mockito.when;
 /*un test qui s'assure que l'insertion dans la base de données fonctionne correctement ;
 	un test qui s'assure que la récupération de la liste des villes fonctionne correctement ;
 	un test qui s'assure que l'endpoint de healthcheck fonctionne correctement.*/
-
+@SpringBootTest(classes = {CtcdApplication.class, CityController.class})
+@AutoConfigureMockMvc
 public class CityControllerTest {
     @Mock
     private CityService cityService;
