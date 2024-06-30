3cf6f101f92047e6d3c4fb5f7dd72406a1e78dae
pauline <pauline.contat00@gmail.com>
2024-06-30 16:49:10 +0200

    remove dotenv

--- src/main/java/org/ghuh/ctcd/CtcdApplication.java
+++ src/main/java/org/ghuh/ctcd/CtcdApplication.java
@@ -1,21 +1,12 @@
 package org.ghuh.ctcd;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Objects;
 
 @SpringBootApplication
 public class CtcdApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		/*System.setProperty("CITY_API_PORT", dotenv.get("CITY_API_PORT"));
-		System.setProperty("CITY_API_DB_URL", dotenv.get("CITY_API_DB_URL"));
-		System.setProperty("CITY_API_DB_USER", dotenv.get("CITY_API_DB_USER"));
-		System.setProperty("CITY_API_DB_PWD", dotenv.get("CITY_API_DB_PWD"));
-		System.setProperty("CITY_API_ADDR", dotenv.get("CITY_API_ADDR"));*/
 		SpringApplication.run(CtcdApplication.class, args);
 	}
 
