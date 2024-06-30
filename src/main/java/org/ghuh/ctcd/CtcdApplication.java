53dcd6e769dd91ec75d118637801616e1896f82b
pauline <pauline.contat00@gmail.com>
2024-06-30 16:20:56 +0200

    add profile test + application.properties for profile test

--- src/main/java/org/ghuh/ctcd/CtcdApplication.java
+++ src/main/java/org/ghuh/ctcd/CtcdApplication.java
@@ -11,11 +11,11 @@ public class CtcdApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
-		System.setProperty("CITY_API_PORT", dotenv.get("CITY_API_PORT"));
+		/*System.setProperty("CITY_API_PORT", dotenv.get("CITY_API_PORT"));
 		System.setProperty("CITY_API_DB_URL", dotenv.get("CITY_API_DB_URL"));
 		System.setProperty("CITY_API_DB_USER", dotenv.get("CITY_API_DB_USER"));
 		System.setProperty("CITY_API_DB_PWD", dotenv.get("CITY_API_DB_PWD"));
-		System.setProperty("CITY_API_ADDR", dotenv.get("CITY_API_ADDR"));
+		System.setProperty("CITY_API_ADDR", dotenv.get("CITY_API_ADDR"));*/
 		SpringApplication.run(CtcdApplication.class, args);
 	}
 
